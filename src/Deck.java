5b17919c354b8618b2393750310086790c165c88
BT_1N3_29 <zchen5143@bths.edu>
2019-02-14 14:44:35 -0500

    FISNAFJD

--- src/Deck.java
+++ src/Deck.java
@@ -34,6 +34,12 @@ public class Deck {
      */
     public Deck(String[] ranks, String[] suits, int[] values) {
 		/* *** TO BE IMPLEMENTED IN ACTIVITY 2 *** */
+        this.cards= new ArrayList<Card>();
+        for(int i=0; i<ranks.length; i++)
+        {
+            cards.add(new Card(ranks[i], suits[i], values[i]));
+        }
+        this.size= ranks.length;
     }
 
 
